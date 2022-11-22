8cce76ff3a507718e55e4f4c7aed5dbe7ff10b1e
Souvik Mitra <mitrasouvik123@gmail.com>
2022-11-22 13:06:37 +0530

    added sign in and sign out func

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Box, Button, Container, HStack, Input, VStack } from "@chakra-ui/react";
 import Message from "./Components/Message";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { onAuthStateChanged, getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import { app } from "./Firebase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const auth = getAuth(app);
@@ -13,48 +13,69 @@ const loginHandler = () => {
   signInWithPopup(auth, provider)
 }
 
+const logoutHandler = () => {
+  signOut(auth)
+}
+
 
 function App() {
   const [user, setUser] = useState(false);
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (data) => {
+      setUser(data)
+    })
+
+    return () => {
+      unsubscribe();
+    }
+  })
+
   return (
     <Box bgColor={"red.100"}>
 
-      <Container bg={"white"} h={"100vh"}>
-        <VStack h={"full"} p={'4'}>
-          <Button colorScheme={'whatsapp'} w={"full"}>Sign Out</Button>
-          <VStack h="full" w={'full'} overflowY='auto'>
+      {
+        user ? (<Container bg={"white"} h={"100vh"}>
+          <VStack h={"full"} p={'4'}>
+            <Button onClick={logoutHandler} colorScheme={'whatsapp'} w={"full"}>Sign Out</Button>
+            <VStack h="full" w={'full'} overflowY='auto'>
 
-            <Message text={"sample message"} />
-            <Message text={"sample message"} user={"me"} />
+              <Message text={"sample message"} />
+              <Message text={"sample message"} user={"me"} />
 
 
 
 
+
+
+            </VStack>
+
+            <form style={{
+
+              width: "100%"
+            }}>
+              <HStack>
+                <Input placeholder="Enter a message..." />
+                <Button colorScheme={'whatsapp'} type="submit">Send</Button>
+              </HStack>
+
+            </form>
 
 
           </VStack>
 
-          <form style={{
-
-            width: "100%"
-          }}>
-            <HStack>
-              <Input placeholder="Enter a message..." />
-              <Button colorScheme={'whatsapp'} type="submit">Send</Button>
-            </HStack>
-
-          </form>
-
-
-        </VStack>
 
 
 
 
+        </Container>) :
+          <VStack h={'100vh'} justifyContent={'center'}>
+            <Button onClick={loginHandler} colorScheme={'whatsapp'}>
+              Sign in with Google
+            </Button>
+          </VStack>
 
-      </Container>
-
+      }
 
 
     </Box>
